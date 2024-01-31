3291457e079f5c1a11949bb0f0bf0d51d7db14ea
mjohank <mjohank@gmail.com>
2024-01-31 13:33:21 +0100

    Updated blog post cards
    - Removed category description from post card
    - Formatted date on post cards for better UX and UI

--- js/utility-scripts/createPostCards.js
+++ js/utility-scripts/createPostCards.js
@@ -1,25 +1,26 @@
 export function createPostCards(posts, container) {
   posts.forEach((post) => {
+    const date = new Date(post.date);
+    const formattedDate = date.toLocaleDateString(undefined, {
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit",
+    });
+
     const postCardHTML = `
       <a href="post-specific.html?id=${post.id}" class="post-card slide bs br">
 
-        <img src="${post.acf.post_thumbnail_image}" alt="${post.acf.slug}" class="post-card-img"/>
+        <img src="${post.acf.post_thumbnail_image}" alt="${post.acf.post_paragraph_6}" class="post-card-img"/>
 
         <div class="post-card-meta-container">
           <h3 class="post-card-heading">${post.title.rendered}</h3>
-          
-          <div class="post-txt-date-container txt">
-            <div class="category-date-container">
-              <p class="post-tag"><span>Category: </span>Coffee type</p>
-              
-              <p class="posted-date">Posted: ${post.acf.date}</p>
-            </div>
-          </div>
+            
+          <p class="posted-date txt">Posted: ${formattedDate}</p>
+            
         </div>
       </a>
     `;
 
-    // Append the generated HTML to the specified container
     container.innerHTML += postCardHTML;
   });
 }
