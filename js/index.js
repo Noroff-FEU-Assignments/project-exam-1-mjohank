24df3071f0ba10a7b57a4c6f3edf1915edef4950
mjohank <mjohank@gmail.com>
2024-02-04 00:44:52 +0100

    Update js for fetching and displaying homepage content
    - Refactored js for creating hero content and welcome section.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,24 @@
-//
-//
+// //
+// //
 // IMPORTS
-
-import { fetchAndDisplayHeroContent } from "./components/heroSection.js";
+import { homePageURL, acfFormat } from "./utility-scripts/constants.js"; // Adjust path as necessary
+import { createHeroContent } from "./components/heroSection.js";
+import { createWelcomeContent } from "./components/welcomeSection.js";
 import { fetchAndDisplayCarouselSlides } from "./components/latestPostsCarousel.js";
 import { fetchAndDisplayRandomQuote } from "./components/quotesSection.js";
 
-document.addEventListener("DOMContentLoaded", function () {
-  fetchAndDisplayHeroContent();
-  fetchAndDisplayCarouselSlides();
-  fetchAndDisplayRandomQuote();
-});
+async function fetchHomePageContent() {
+  try {
+    const response = await fetch(homePageURL + acfFormat);
+    const homeJSON = await response.json();
+
+    createHeroContent(homeJSON);
+    createWelcomeContent(homeJSON);
+    fetchAndDisplayCarouselSlides();
+    fetchAndDisplayRandomQuote();
+  } catch (error) {
+    console.error("Error fetching home page content:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", fetchHomePageContent);
