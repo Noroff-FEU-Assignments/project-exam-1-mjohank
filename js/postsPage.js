9a078b0622ddd3ed1df30d0708432a9d67c02b88
mjohank <mjohank@gmail.com>
2024-02-06 12:18:44 +0100

    Removed commented out, redundant  code

--- js/postsPage.js
+++ js/postsPage.js
@@ -1,46 +1,5 @@
-// //IMPORTS
-// import { fetchPosts } from "./api/fetchPosts.js";
-// import { createPostCards } from "./utility-scripts/createPostCards.js";
-// import { baseURL, acfFormat, pagination } from "./utility-scripts/constants.js";
-
-// //CONSTANTS
-// const bloglistContainer = document.querySelector(".bloglist-container");
-// const viewMoreBtn = document.querySelector(".view-more-btn");
-
-// let page = 1; // Initial page
-
-// // FUNCTION TO FETCH AND DISPLAY POSTS AS CARDS IN SETS OF 9
-// async function fetchAndDisplayPosts() {
-//   try {
-//     const posts = await fetchPosts(page);
-//     createPostCards(posts, bloglistContainer);
-
-//     // Check if there are more posts, if not inform user
-//     if (posts.length < 9) {
-//       viewMoreBtn.textContent = "End of Content";
-//       viewMoreBtn.disabled = true;
-//       viewMoreBtn.style.border = "2px solid var(--clr-main-acc)";
-//       viewMoreBtn.style.backgroundColor = "var(--clr-sec-background)";
-//     }
-//   } catch (error) {
-//     console.error("Error fetching and displaying posts: ", error);
-//   }
-// }
-
-// // Function to handle "View More" button click
-// function handleViewMoreClick() {
-//   page++;
-//   fetchAndDisplayPosts();
-// }
-
-// // Event listener for "View More" button click
-// viewMoreBtn.addEventListener("click", handleViewMoreClick);
-
-// // Initial fetch and display of posts
-// fetchAndDisplayPosts();
-
-/////////////////////////////////////////////////////
-
+//
+//
 //IMPORTS
 import { fetchPosts } from "./api/fetchPosts.js";
 import { createPostCards } from "./utility-scripts/createPostCards.js";
@@ -54,15 +13,13 @@ let page = 1; // Initial page
 async function fetchAndDisplayPosts() {
   try {
     const posts = await fetchPosts(page);
-    let displayPosts = [...posts]; // Clone the posts array to manipulate
+    let displayPosts = [...posts]; // Cloning the posts array to manipulate
 
-    // Adjust the number of posts based on viewport width
+    // Accounting for "grid-template-columns: repeat(2, 1fr)" on medium size viewports"
     if (window.innerWidth >= 768 && window.innerWidth <= 1200) {
-      // Assuming you prefer 8 for medium screens to keep rows even
       if (posts.length === 9) {
-        displayPosts.pop(); // Remove the last post to make it 8
+        displayPosts.pop(); // Removing the last post to make the number of posts even
       } else if (posts.length > 9) {
-        // Optionally adjust to 8 or another suitable number for your layout
         displayPosts = posts.slice(0, 8); // Adjust as needed
       }
     }
@@ -81,7 +38,7 @@ async function fetchAndDisplayPosts() {
   }
 }
 
-// Function to handle "View More" button click
+// Handling "View More" button click
 function handleViewMoreClick() {
   page++;
   fetchAndDisplayPosts();
