6d60f20b4347d389bd91680514a281049c4854fe
mjohank <mjohank@gmail.com>
2024-02-10 19:16:49 +0100

    Update errorMessage.js
    Added error message specific to form input fields

--- js/components/errorMessage.js
+++ js/components/errorMessage.js
@@ -4,3 +4,7 @@ export function errorMessage(
 ) {
   return `<div class="error-handling">${message}: ${error}</div>`;
 }
+
+export function contactFormInputErrorFeedback() {
+  return `<p class="form-error">Oops..something went wrong. Please check the input fields above <i class="fa-regular fa-hand-point-up contact-finger-up "></i></p>`;
+}
